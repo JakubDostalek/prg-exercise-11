3263501976b991dd24b27ee087c65b1866c37ba2
Jakub Dostalek <dostalek.jakub09@gmail.com>
2026-04-23 15:39:58 +0200

    prvni

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
@@ -23,13 +24,25 @@ def selection_sort(seznam_start):
 def bubble_sort(seznam_start):
     cisla = seznam_start.copy()
     c = len(cisla)
+    plt.ion()
+    plt.show()
 
     for i in range(c):
         for j in range(c - i - 1):
+            index_highlight1 = j
+            index_highlight2 = j + 1
+            colors = ["steelblue"] * len(cisla)
+            colors[index_highlight1] = "tomato"
+            colors[index_highlight2] = "tomato"
+            plt.clf()
+            plt.bar(range(len(cisla)), cisla, color=colors)
+            plt.title("Bubble Sort")
+            plt.pause(0.1)
             if cisla[j] > cisla[j + 1]:
 
                 cisla[j], cisla[j + 1] = cisla[j + 1], cisla[j]
-
+    plt.ioff()
+    plt.show()
     return cisla
 
 if __name__ == "__main__":
@@ -45,5 +58,7 @@ if __name__ == "__main__":
     print(f"test selection sort: {selection_sort(test)}")
     print(f"test bubble sort: {bubble_sort(test)}")
 
+    bubble = bubble_sort(random_numbers(10))
+    print(bubble)
     # small = random_numbers(5, low=0, high=20)  # 5 čísel v rozsahu 0–20
     # print(selection_sort(random_numbers(14)))
